f06b2ddcebebbbc84ef2da9cb31125c3abd1eade
istredd <labno.b@gmail.com>
2018-05-22 08:53:19 +0100

    vault typo

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -37,7 +37,7 @@ module "sscs-job-scheduler" {
 
     // IdAM s2s
     S2S_URL = "${local.s2sCnpUrl}"
-    S2S_SECRET = "${data.vault_generic_secret.s2s_secret.data["value"]}"
+    S2S_SECRET = "${data.vault_generic_secret.sscs_s2s_secret.data["value"]}"
     S2S_MICROSERVICE = "${var.idam_s2s_auth_microservice}"
 
     // db vars
