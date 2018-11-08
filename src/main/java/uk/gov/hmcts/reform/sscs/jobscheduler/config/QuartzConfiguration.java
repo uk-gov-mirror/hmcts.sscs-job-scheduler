516bca2ab283c780ef8c4686c0bad53e615aaa7d
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-11-08 09:38:20 +0000

    Make auto starting Quartz configurable

--- src/main/java/uk/gov/hmcts/reform/sscs/jobscheduler/config/QuartzConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/jobscheduler/config/QuartzConfiguration.java
@@ -51,6 +51,7 @@ public class QuartzConfiguration {
     @Singleton
     public Scheduler scheduler(
         SchedulerFactoryBean factory,
+        @Value("${job.scheduler.autoStart}") boolean autoStart,
         @Value("${job.scheduler.retryPolicy.maxNumberOfJobExecutions}") int maxJobExecutionAttempts,
         @Value("${job.scheduler.retryPolicy.delayBetweenAttemptsInMs}") long delayBetweenAttemptsInMs
     ) throws SchedulerException {
@@ -63,7 +64,10 @@ public class QuartzConfiguration {
         );
 
         scheduler.getListenerManager().addJobListener(failedJobRescheduler);
-        scheduler.start();
+
+        if (autoStart) {
+            scheduler.start();
+        }
 
         return scheduler;
     }
