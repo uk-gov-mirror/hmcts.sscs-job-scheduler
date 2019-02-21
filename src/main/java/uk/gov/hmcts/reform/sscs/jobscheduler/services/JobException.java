5c36fda3499be72fd8f8d1c466a612568cb32915
jackmaloney <jack.maloney@transformuk.com>
2019-02-21 11:46:39 +0000

    Change exceptions to extend runtime exceptions

--- src/main/java/uk/gov/hmcts/reform/sscs/jobscheduler/services/JobException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/jobscheduler/services/JobException.java
@@ -1,15 +1,12 @@
 package uk.gov.hmcts.reform.sscs.jobscheduler.services;
 
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
-
 /**
  * SonarQube reports as error. Max allowed - 5 parents
  */
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-public class JobException extends UnknownErrorCodeException {
+public class JobException extends RuntimeException {
 
     public JobException(String message, Throwable cause) {
-        super(AlertLevel.P1, message, cause);
+        super(message, cause);
     }
 }
--- src/main/java/uk/gov/hmcts/reform/sscs/jobscheduler/services/JobNotFoundException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/jobscheduler/services/JobNotFoundException.java
@@ -1,15 +1,12 @@
 package uk.gov.hmcts.reform.sscs.jobscheduler.services;
 
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
-
 /**
  * SonarQube reports as error. Max allowed - 5 parents
  */
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-public class JobNotFoundException extends UnknownErrorCodeException {
+public class JobNotFoundException extends RuntimeException {
 
     public JobNotFoundException(String message) {
-        super(AlertLevel.P4, message);
+        super(message);
     }
 }
